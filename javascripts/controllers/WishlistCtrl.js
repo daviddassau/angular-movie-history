1562b5956d654974365aa2806213a9c8a5a5a3dc
David Dassau <todieahero@hotmail.com>
2017-11-15 21:45:03 -0600

    movies deleting

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -2,10 +2,22 @@
 
 app.controller("WishlistCtrl", function ($rootScope, $scope, MovieService) {
     
-    MovieService.getWishlistMovies($rootScope.uid).then((results) => {
-        $scope.movies = results;
-    }).catch((error) => {
-        console.log("error in getRatedMovies", error);
-    });
+    const getMovies = () => {
+        MovieService.getWishlistMovies($rootScope.uid).then((results) => {
+            $scope.movies = results;
+        }).catch((error) => {
+            console.log("error in getRatedMovies", error);
+        });
+    };
+
+    getMovies();
+
+    $scope.deleteMovie = (movieId) => {
+        MovieService.deleteMovie(movieId).then((result) => {
+            getMovies();
+        }).catch((error) => {
+            console.log("error in deleteMovie", error);
+        });
+    };
 
 });
