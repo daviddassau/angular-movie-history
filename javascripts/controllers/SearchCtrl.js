8326d19ca156dbf36bb2be46bf4f217f0ef6e876
David Dassau <todieahero@hotmail.com>
2017-11-15 20:56:46 -0600

    you can now add movies to rated

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("SearchCtrl", function ($scope, tmdbService) {
+app.controller("SearchCtrl", function ($location, $rootScope, $scope, MovieService, tmdbService) {
     $scope.movies = [];
 
     $scope.enterPush = (event) => {
@@ -11,6 +11,26 @@ app.controller("SearchCtrl", function ($scope, tmdbService) {
                 console.log("error in searchMovies", err);
             });
         }
+    };
+
+    const createMovie = (movie) => {
+        return {
+            "title": movie.title,
+            "overview": movie.overview,
+            "poster_path": movie.poster_path,
+            "rating": 0,
+            "isWatched": true,
+            "uid": $rootScope.uid
+        };
+    };
+
+    $scope.saveRated = (tmdbMovie) => {
+        let newMovie = createMovie(tmdbMovie);
+        MovieService.postNewMovie(newMovie).then(() => {
+            $location.path('/rated');
+        }).catch((error) => {
+            console.log("error in postNewMovie", error);
+        });
     };
 
 });
--- javascripts/services/MovieService.js
+++ javascripts/services/MovieService.js
@@ -36,7 +36,11 @@ app.service("MovieService", function ($http, $q, FIREBASE_CONFIG){
                 reject(error);
             });
         });
-    };    
+    };
+    
+    const postNewMovie = (newMovie) => {
+        return $http.post(`${FIREBASE_CONFIG.databaseURL}/movies.json`, JSON.stringify(newMovie));
+    };
 
-    return {getRatedMovies, getWishlistMovies};
+    return {getRatedMovies, getWishlistMovies, postNewMovie};
 });
