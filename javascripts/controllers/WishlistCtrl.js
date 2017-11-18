bf1fe9eec9d0e172a128e4f5860b1ac21b0aca06
David Dassau <todieahero@hotmail.com>
2017-11-18 09:48:36 -0600

    movie now added to rated page

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -22,9 +22,9 @@ app.controller("WishlistCtrl", function ($rootScope, $scope, MovieService) {
 
     $scope.switchWatched = (movie) => {
         movie.isWatched = true;
-        console.log("movie", movie);
-        MovieService.updateMovie(movie, movie.id).then((result) => {
-            console.log("result", result);
+        let updatedMovie = MovieService.createMovieObject(movie);
+        MovieService.updateMovie(updatedMovie, movie.id).then((result) => {
+            getMovies();
         }).catch((error) => {
             console.log("error in updateMovie", error);
         });
